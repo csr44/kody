29108ac43f952633802a221275e341d79d53a315
csr44 <55451248+csr44@users.noreply.github.com>
2021-08-05 13:43:55 +0200

    Update CameraControlSystem.py

--- Automatic positioning system/CameraControlSystem.py
+++ Automatic positioning system/CameraControlSystem.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 import time
 import RPi.GPIO as gpio
-vid = cv2.VideoCapture(0)
-vid.set(3, 320)
+vid = cv2.VideoCapture(0) #objekt vid
+vid.set(3, 320) #znizenie rozlisenia kvoli vypoctovej narocnosti
 vid.set(4, 240)
 X=320
-Y=240
-centerX = int(X/2)
+Y=240 
+centerX = int(X/2) # definovanie stredu obrazovky
 centerY = int(Y/2)
-R3=1 #MUSI BYT NEPARNE CISLO
-behXmotora=False
+R3=1 #
+behXmotora=False # Boolean pomocne premenne pre korektnu cinnost motorov
 behYmotora=False
 citlivost=1
 VMplus=False
@@ -18,7 +18,7 @@ VMmin=False
 HMplus=False
 HMmin=False
 i=1
-def Stop():
+def Stop(): #nevyuzita funkcia - dalsi spôsob riadenia
     if VMplus == True and RoY<=0:
         print("STOP")
     if VMmin == True and RoY>=0:
@@ -30,34 +30,34 @@ def Stop():
 
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
-gpio.setup(27, gpio.OUT, initial=gpio.LOW)
+gpio.setup(27, gpio.OUT, initial=gpio.LOW) #pri inicializacii systemu su oba motory uzemnene - prostredníctvom relé 
 gpio.setup(22, gpio.OUT, initial=gpio.LOW)
 gpio.setup(23, gpio.OUT, initial=gpio.LOW)
 gpio.setup(24, gpio.OUT, initial=gpio.LOW)
 #zaciatok=time.time()
 while(True):
     ret, img = vid.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gauss = cv2.GaussianBlur(gray, (R3, R3), 0)
-    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(gauss)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # prevod do monochromatickej stupnice sedi - takto ma kazdy pixel definovanu hodnotu podla intenzity ziarenia
+    gauss = cv2.GaussianBlur(gray, (R3, R3), 0)  # Gaussiansky filter pre odstranenie zkreslenia a neziaducich detailov 
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(gauss) # Najdena lokacia (x,y) najjasnejsieho pixela 
     x,y=maxLoc
-    tval=0.98*maxVal
-    ret,thresh = cv2.threshold(gauss,tval,255,cv2.THRESH_BINARY)
-    pocet=cv2.countNonZero(thresh)
-    x_center, y_center= np.argwhere(thresh==255).sum(0)
-    Xx = int(y_center/pocet)
+    tval=0.98*maxVal # znizenie maximalnej hodnoty najjasnejsieho pixela 
+    ret,thresh = cv2.threshold(gauss,tval,255,cv2.THRESH_BINARY) # Prevod do binárnej podoby (prahová hodnota urcena podla premennej tval) - biela zostane iba cast najjasnejsich pixelov
+    pocet=cv2.countNonZero(thresh) #spocitanie nenulovych pixelov 
+    x_center, y_center= np.argwhere(thresh==255).sum(0) #sucet pozdlz riadkov
+    Xx = int(y_center/pocet) #vypocitany stred z bielych pixelov
     Yy = int(x_center/pocet)
-    cv2.circle(img, (Xx, Yy), 3, (0, 0, 255), -1)
-    cv2.circle(img, (centerX, centerY), 3, (0, 255, 255), -1)
+    cv2.circle(img, (Xx, Yy), 3, (0, 0, 255), -1) #kruh v strede bielych pixelov
+    cv2.circle(img, (centerX, centerY), 3, (0, 255, 255), -1) #kruh v strede obrazovky
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     #cv2.imshow('image', thresh)
-    cv2.imshow('real', img)
+    cv2.imshow('real', img) #vyobrazenie jednotlivych filtrov do okien
     cv2.imshow('binar', thresh)
     cv2.imshow('rozmazanie', gauss)
     #Motory
-    RoY = centerY - Yy 
-    if (abs(RoY)>citlivost or behYmotora==True) and behXmotora!=True:
+    RoY = centerY - Yy #rozdiel stredu obrazovky a stredu bieleho objektu na ose X
+    if (abs(RoY)>citlivost or behYmotora==True) and behXmotora!=True: #Zapnutie polohovania vertikalneho motora, horizontalny teraz nepolohuje, po dosiahnuti stredu (RoY=~0) sa motor vypne
         if RoY>0 and behYmotora!=True and abs(RoY)>citlivost:#DOPREDU
             behYmotora = True
             gpio.output(27, gpio.HIGH)
@@ -71,7 +71,7 @@ while(True):
                 #VMmin=True
                 #Stop()
 
-        if behYmotora == True and abs(RoY)<citlivost:
+        if behYmotora == True and abs(RoY)<citlivost:#Vypni
             #ZASTAVENIE 
             behYmotora = False
             gpio.output(27, gpio.LOW)
@@ -79,7 +79,7 @@ while(True):
     
     RoX = centerX - Xx 
     
-    if (abs(RoX)>citlivost or behXmotora==True) and behYmotora!=True:
+    if (abs(RoX)>citlivost or behXmotora==True) and behYmotora!=True: #Zapnutie polohovania horizontalneho motora
         if RoX>0 and behXmotora!=True and abs(RoX)>citlivost:#DOPREDU
             behXmotora = True
             gpio.output(23, gpio.HIGH)
@@ -93,7 +93,7 @@ while(True):
             #HMmin=True
         #Stop()
         
-        if behXmotora == True and abs(RoX)<citlivost:
+        if behXmotora == True and abs(RoX)<citlivost: #Vypni
         #ZASTAVENIE POHYBOV VERTIKALNEHO
             behXmotora = False
             gpio.output(23, gpio.LOW)
